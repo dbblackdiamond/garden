eb3e6b6c4629434ae3a67e96ee1cdfd2602c5383
dbblackdiamond <bertrand_sirodot@leobigfoot.com>
2019-09-05 22:14:12 -0600

    Adding redis

--- datastructures.go
+++ datastructures.go
@@ -20,3 +20,38 @@ type Record struct {
 }
 
 type Records []Record
+
+type Sensor struct {
+  Number int `json:"slot"`
+  Dots []Dot `json:"dots"`
+  Water int `json:"water"`
+}
+
+type Sensors []Sensor
+
+type Dot struct {
+  Timestamp int64 `json:"timestamp"`
+  Value float64   `json:"value"`
+}
+
+type UbidotsAPI struct {
+  Count bool  `json:"count"`
+  Next string `json:"next"`
+  Previous string `json:"previous"`
+  Results []Dot `json:"results"`
+}
+
+type Device struct {
+  Key string
+  Id string       `json:"device" redis:"id"`
+  NumSensors int  `json:"sensors" redis:"sensors"`
+  Box int         `json:"box" redis:"box"`
+  Watering int    `json:"watering" redis:"watering"`
+  Valve int       `json:"valve" redis:"valve"`
+  Sleep int       `json:"sleep" redis:"sleep"`
+}
+
+type Garden struct {
+  Devices []Device `json:"garden"`
+  NumDevices int  `json:"devices"`
+}
